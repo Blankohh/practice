d17c5642dc5cd7cb6b40e2608f87fa0bdd7d9dab
Blank <1845011776@qq.com>
2026-09-01 18:11:10 +0800

    feat: two sum practice

---
+++ test_two_sum.py
@@ -0,0 +1,21 @@
+﻿from two_sum import has_pair_sum
+
+
+def test_found():
+    assert has_pair_sum([1, 2, 3, 4], 5) is True
+
+
+def test_not_found():
+    assert has_pair_sum([1, 2, 3, 4], 9) is False
+
+
+def test_empty():
+    assert has_pair_sum([], 1) is False
+
+
+def test_single():
+    assert has_pair_sum([2], 4) is False
+
+
+def test_two_ones():
+    assert has_pair_sum([1, 1, 2, 3], 2) is True
---
+++ two_sum.py
@@ -0,0 +1,12 @@
+﻿def has_pair_sum(nums, target):
+    left = 0
+    right = len(nums) - 1
+    while left < right:
+        total = nums[left] + nums[right]
+        if total == target:
+            return True
+        elif total < target:
+            left += 1
+        else:
+            right -= 1
+    return False
