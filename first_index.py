30522dbde6d1ba8e72ebdbfb65c0352e320d65e7
Blank <1845011776@qq.com>
2026-09-01 22:11:16 +0800

    feat: first index practice

---
+++ first_index.py
@@ -0,0 +1,14 @@
+﻿def first_index(nums, target):
+    left = 0
+    right = len(nums) - 1
+    result = -1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            result = mid
+            right = mid - 1
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return result
---
+++ test_first_index.py
@@ -0,0 +1,16 @@
+﻿from first_index import first_index
+
+def test_repeated():
+        assert first_index([1,2,2,2,3],2) == 1
+
+def test_not_found():
+        assert first_index([1,2,3],4) == -1
+
+def test_all_same():
+        assert first_index([2,2,2],2) == 0
+
+def test_single():
+        assert first_index([5],5) == 0
+
+def test_empty():
+        assert first_index([],1) == -1
